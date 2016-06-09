2e0d32899493747a66a5626f273c6335579703ff
Griffin <griffinschneider@gmail.com>
2016-06-08 22:31:26 -0400

    Initial Commit

---
+++ tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tracker
+//
+//  Created by Griffin on 6/8/16.
+//  Copyright © 2016 griff.zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tracker
+//
+//  Created by Griffin on 6/8/16.
+//  Copyright © 2016 griff.zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
