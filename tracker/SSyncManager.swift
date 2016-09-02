82d631f20f6863341a60512cc921971720d19e5e
Griffin Schneider <griffin@gasbuddy.com>
2016-09-02 12:18:04 -0400

    [Swift] When data is modified, write it to disk in a background thread.

--- tracker/SSyncManager.swift
+++ tracker/SSyncManager.swift
@@ -12,9 +12,23 @@ import RxSwift
 
 class SSyncManager {
     static var schema = Variable(Mapper<SSchema>().map(try! NSString(contentsOfURL: schemaPath, encoding: NSUTF8StringEncoding))!)
-    static var data = Variable(Mapper<SData>().map(try! NSString(contentsOfURL: dataPath, encoding: NSUTF8StringEncoding))!)
+    
+    static var data:Variable<SData> = {
+        let data = Variable(Mapper<SData>().map(try! NSString(contentsOfURL: dataPath, encoding: NSUTF8StringEncoding))!)
+        _ = data.asObservable()
+            .skip(1)
+            .observeOn(SerialDispatchQueueScheduler(internalSerialQueueName: "DataWriteQueue"))
+            .subscribeNext {
+                try! $0.toJSONString(true)!.writeToURL(
+                    SSyncManager.dataPath,
+                    atomically: true,
+                    encoding: NSUTF8StringEncoding
+                )
+        }
+        return data
+    }()
     
     private static let containerPath = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.zone.griff.tracker")!
-    private static var schemaPath = containerPath.URLByAppendingPathComponent("schema.json")
-    private static var dataPath = containerPath.URLByAppendingPathComponent("data.json")
+    private static let schemaPath = containerPath.URLByAppendingPathComponent("schema.json")
+    private static let dataPath = containerPath.URLByAppendingPathComponent("data.json")
 }
